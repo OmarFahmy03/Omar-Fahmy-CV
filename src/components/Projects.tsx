c541bb115c4264ad825d47a4bb8fc0344af79ac3
OmarFahmy03 <omarhussain755@gmail.com>
2025-10-21 18:30:54 +0300

    -Update when link is empty

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -203,14 +203,16 @@ const Projects: React.FC = () => {
               <p className="text-sm text-[#B6B6B6] mb-4">{project.description}</p>
               
               <div className="Buttons flex gap-2">
-                <a
-                  href={project.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="px-4 py-2 bg-blue-500 hover:bg-[#07c7c4]/80 text-white rounded hover:text-white hover:outline-[#08D9D6] outline-1 transition-all duration-300"
-                >
-                  View Project
-                </a>
+                {project.link && (
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-blue-500 hover:bg-[#07c7c4]/80 text-white rounded hover:text-white hover:outline-[#08D9D6] outline-1 transition-all duration-300"
+                  >
+                    View Project
+                  </a>
+                )}
 
                 {project.code && (
                   <a
@@ -234,11 +236,10 @@ const Projects: React.FC = () => {
                       <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/>
                       <path d="M9 18c-4.51 2-5-2-7-2"/>
                     </svg>
-                    <span className='ms-1'>Source Code</span>
+                    <span className="ms-1">Source Code</span>
                   </a>
                 )}
               </div>
-
             </div>
           ))}
         </div>
